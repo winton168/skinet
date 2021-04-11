3baff781a0bcebcf79f8c35e988c50dff4edfdd7
Winton Wang <wwang@cmigroup.com>
2021-04-11 16:11:16 -0400

    End of section

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { IProduct } from '../app/models/product';
-import { IPagination  } from './models/pagination';
-
 
 @Component({
   selector: 'app-root',
@@ -12,19 +8,12 @@ import { IPagination  } from './models/pagination';
 export class AppComponent implements OnInit {
 
   title = 'Skinet';
-  products: IProduct[];
+
   
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
     
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe( (response : IPagination ) => {
-     // console.log(response);
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
-
   }
 
 
